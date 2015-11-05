b2667003f6b2b26c883c7fa5aaeb657285fea3ab
Wojciech Drazkiewicz <troll12345@wp.pl>
2015-11-05 10:28:23 +0100

    Wstawienie projektu

---
+++ src/bazydanych/Employee.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bazydanych;
+
+/**
+ *
+ * @author drazkiew
+ */
+import java.util.*;
+
+public class Employee {
+   private int id;
+   private String firstName; 
+   private String lastName;   
+   private int salary;
+   private Set certificates;
+
+   public Employee() {}
+   public Employee(String fname, String lname, int salary) {
+      this.firstName = fname;
+      this.lastName = lname;
+      this.salary = salary;
+   }
+   public int getId() {
+      return id;
+   }
+   public void setId( int id ) {
+      this.id = id;
+   }
+   public String getFirstName() {
+      return firstName;
+   }
+   public void setFirstName( String first_name ) {
+      this.firstName = first_name;
+   }
+   public String getLastName() {
+      return lastName;
+   }
+   public void setLastName( String last_name ) {
+      this.lastName = last_name;
+   }
+   public int getSalary() {
+      return salary;
+   }
+   public void setSalary( int salary ) {
+      this.salary = salary;
+   }
+
+   public Set getCertificates() {
+      return certificates;
+   }
+   public void setCertificates( Set certificates ) {
+      this.certificates = certificates;
+   }
+}
---
+++ src/bazydanych/JDBCExample.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bazydanych;
+
+/**
+ *
+ * @author drazkiew
+ */
+//STEP 1. Import required packages
+import java.sql.*;
+
+public class JDBCExample {
+   // JDBC driver name and database URL
+   static final String JDBC_DRIVER = "org.postgresql.Driver";  
+   static final String DB_URL = "jdbc:postgresql://localhost/baza";
+
+   //  Database credentials
+   static final String USER = "user";
+   static final String PASS = "password";
+   
+   public static void main(String[] args) {
+   Connection conn = null;
+   Statement stmt = null;
+   try{
+      //STEP 2: Register JDBC driver
+      Class.forName("org.postgresql.Driver");
+
+      //STEP 3: Open a connection
+      System.out.println("Connecting to database...");
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+      //STEP 4: Execute a query
+      System.out.println("Creating database...");
+      stmt = conn.createStatement();
+      
+      String sql = "CREATE DATABASE BAZA";
+      stmt.executeUpdate(sql);
+      System.out.println("Database created successfully...");
+   }catch(SQLException se){
+      //Handle errors for JDBC
+      se.printStackTrace();
+   }catch(Exception e){
+      //Handle errors for Class.forName
+      e.printStackTrace();
+   }finally{
+      //finally block used to close resources
+      try{
+         if(stmt!=null)
+            stmt.close();
+      }catch(SQLException se2){
+      }// nothing we can do
+      try{
+         if(conn!=null)
+            conn.close();
+      }catch(SQLException se){
+         se.printStackTrace();
+      }//end finally try
+   }//end try
+   System.out.println("Goodbye!");
+}//end main
+}//end JDBCExample
