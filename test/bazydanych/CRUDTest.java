63e56f7e249355c79a338c5e03402b3859dfd598
Wojciech Drazkiewicz <troll12345@wp.pl>
2015-11-13 13:06:17 +0100

    Testy - struktura

---
+++ test/bazydanych/CRUDTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bazydanych;
+
+import org.junit.Test;
+import org.junit.Before;
+
+/**
+ *
+ * @author Wojtek
+ */
+public class CRUDTest {
+
+    public CRUDTest() {
+    }
+
+    @Test
+    public void CreateTest() {
+        assert (true);
+    }
+
+    @Before
+    public void ReadTest() {
+        assert (true);
+    }
+
+    @Test
+    public void UpdateTest() {
+        assert (true);
+    }
+
+    @Test
+    public void DeleteTest() {
+        assert (true);
+    }
+
+}
---
+++ test/bazydanych/OneToManyTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bazydanych;
+
+import org.junit.Test;
+import org.junit.Before;
+
+/**
+ *
+ * @author Wojtek
+ */
+public class OneToManyTest {
+
+    public OneToManyTest() {
+    }
+    
+    /*TO DO - EVERYTHING*/
+
+}
